0eb35537bdd6d4b35d505f253b098bd35a3b9da3
furantuturu <furantuturu@proton.me>
2026-08-31 15:19:00 +0800

    Relocated the Supabase initialization in getit container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,9 +1,15 @@
 import 'package:get_it/get_it.dart';
 import 'package:talker_flutter/talker_flutter.dart';
+import 'package:mt/config/env/env.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 final sl = GetIt.instance;
 
 void setupLocator() async {
+  //supabase
+  await Supabase.initialize(url: Env.spbUrl, publishableKey: Env.spbPbkey);
+  sl.registerLazySingleton<SupabaseClient>(() => Supabase.instance.client);
+
   // talker
   sl.registerSingleton(TalkerFlutter.init());
   sl.registerSingleton(TalkerRouteObserver(Talker()));
