75610f4d6373ad8a8a32e5c2f188a6f8d3948bc5
furantuturu <furantuturu@proton.me>
2026-09-01 21:52:57 +0800

    Initialized refreshListenable and redirection to listen supabase auth state. Filled up other routes

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mt/core/router/go_router_refresh_stream.dart';
+import 'package:mt/core/router/routes.dart';
+import 'package:mt/features/error/presentation/error404.dart';
+import 'package:mt/features/auth/data/repository/auth_repo_impl.dart';
+import 'package:mt/features/auth/presentation/pages/login.dart';
+import 'package:mt/features/home/presentation/pages/home.dart';
 import 'package:mt/injection_container.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
-final rootNavigatorKey = GlobalKey<NavigatorState>();
+final rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
+final authRepo = AuthRepoImpl(sl<SupabaseClient>());
 
 final goRouter = GoRouter(
   navigatorKey: rootNavigatorKey,
   observers: [sl<TalkerRouteObserver>()],
   initialLocation: '/',
+  refreshListenable: GoRouterRefreshStream(authRepo.authStateChange),
+  redirect: (context, state) {
+    final isLoggedIn = authRepo.isLoggedIn;
+    final isAuthRoute =
+        state.matchedLocation == '/login' ||
+        state.matchedLocation == '/register';
+
+    if (!isLoggedIn && !isAuthRoute) return '/login';
+    if (isLoggedIn && isAuthRoute) return '/';
+
+    return null;
+  },
   routes: [
     GoRoute(
       path: '/',
-      name: 'Home',
+      name: AppRoute.home,
+      builder: (context, state) => Home(),
+    ),
+    GoRoute(
+      path: '/login',
+      name: AppRoute.login,
+      builder: (context, state) => Login(),
+    ),
+    GoRoute(
+      path: '/register',
+      name: AppRoute.register,
       builder: (context, state) => const Scaffold(),
     ),
   ],
+  errorPageBuilder: (context, state) => CustomTransitionPage(
+    key: state.pageKey,
+    child: Error404(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+        FadeTransition(opacity: animation, child: child),
+  ),
 );
