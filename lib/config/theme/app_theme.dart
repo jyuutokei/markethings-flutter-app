e0f25339ee4d9fbbe95168a8669af40ce446e843
furantuturu <furantuturu@proton.me>
2026-09-01 21:53:22 +0800

    Setup color scheme of the app

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -4,6 +4,24 @@ ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: Color(0xff9cd5ff),
     fontFamily: 'Poppins',
+    colorScheme: ThemeData.light().colorScheme.copyWith(
+      primary: Color(0xff046ef4),
+      onPrimary: Colors.white,
+      secondary: Color(0xff077eff),
+      onSecondary: Colors.white,
+    ),
+    textTheme: ThemeData.light().textTheme.apply(
+      bodyColor: Colors.black,
+      displayColor: Colors.black,
+      decorationColor: Colors.black,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      labelStyle: TextStyle(color: Colors.black),
+      hintStyle: TextStyle(color: Colors.black),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(foregroundColor: Colors.black),
+    ),
   );
 }
 
