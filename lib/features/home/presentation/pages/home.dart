6147e47f345fd644c8157ebf41033970b1f20a2e
furantuturu <furantuturu@proton.me>
2026-09-03 21:53:04 +0800

    Initial home page

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mt/core/constants/constants.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Home"));
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset("assets/icons/menu.svg"),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset("assets/icons/Location.svg"),
+            const SizedBox(width: defaultPadding / 2),
+            Text(
+              "15/2 New Texas",
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset("assets/icons/Notification.svg"),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
