84d09fa5d944faf20097301ac4cd94d3a44f1919
furantuturu <furantuturu@proton.me>
2026-09-03 22:01:54 +0800

    Refactor dependency injection for SupabaseClient and AuthRepo

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,4 +1,6 @@
 import 'package:get_it/get_it.dart';
+import 'package:mt/features/auth/data/repository/auth_repo_impl.dart';
+import 'package:mt/features/auth/domain/repository/auth_repo.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 import 'package:mt/config/env/env.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -8,7 +10,8 @@ final sl = GetIt.instance;
 void setupLocator() async {
   //supabase
   await Supabase.initialize(url: Env.spbUrl, publishableKey: Env.spbPbkey);
-  sl.registerLazySingleton<SupabaseClient>(() => Supabase.instance.client);
+  sl.registerSingleton<SupabaseClient>(Supabase.instance.client);
+  sl.registerLazySingleton<AuthRepo>(() => AuthRepoImpl(sl<SupabaseClient>()));
 
   // talker
   sl.registerSingleton<Talker>(TalkerFlutter.init());
