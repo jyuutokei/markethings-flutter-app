0af1d0edae9a628cea478ea91f24474d90c8af1a
furantuturu <furantuturu@proton.me>
2026-09-07 22:39:17 +0800

    Added reset password OTP route to guest-only routes

--- lib/core/router/routes.dart
+++ lib/core/router/routes.dart
@@ -2,9 +2,14 @@ abstract class AppRoute {
   static const login = 'login';
   static const home = 'home';
   static const confirmEmailOtp = 'confirm_email_otp';
+  static const resetPwdOtp = 'reset_pwd_otp';
 }
 
 abstract class AppRouteGuard {
-  static const guestOnlyRoutes = {'/login', '/confirm_email_otp'};
+  static const guestOnlyRoutes = {
+    '/login',
+    '/confirm_email_otp',
+    '/reset_pwd_otp',
+  };
   static const authenticatedOnlyRoutes = {'/'};
 }
