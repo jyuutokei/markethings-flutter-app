38dc2d570f9e21c640b368e22328083123b85ffb
furantuturu <furantuturu@proton.me>
2026-09-10 20:54:26 +0800

    Update route guards to include bridge route and clean up guest-only routes

--- lib/core/router/routes.dart
+++ lib/core/router/routes.dart
@@ -3,13 +3,10 @@ abstract class AppRoute {
   static const home = 'home';
   static const confirmEmailOtp = 'confirm_email_otp';
   static const resetPwdOtp = 'reset_pwd_otp';
+  static const bridge = 'bridge';
 }
 
 abstract class AppRouteGuard {
-  static const guestOnlyRoutes = {
-    '/login',
-    '/confirm_email_otp',
-    '/reset_pwd_otp',
-  };
-  static const authenticatedOnlyRoutes = {'/'};
+  static const guestOnlyRoutes = {'/login', '/confirm_email_otp'};
+  static const authenticatedOnlyRoutes = {'/', '/bridge'};
 }
