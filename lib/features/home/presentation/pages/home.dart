3d4ea34d9d2d915805ce6beab1548b0a9ffc0c51
furantuturu <furantuturu@proton.me>
2026-09-10 20:55:22 +0800

    Refactor Home widget to StatefulWidget and implement loading state with a progress indicator

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -3,12 +3,35 @@ import 'package:flutter_svg/svg.dart';
 import 'package:mt/core/constants/constants.dart';
 import 'package:mt/injection_container.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'dart:async';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      if (mounted) {
+        setState(() => _isLoading = false);
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
