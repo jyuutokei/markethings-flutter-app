6e4c478343b49bb359592284824cff7f5389c434
furantuturu <furantuturu@proton.me>
2026-09-10 20:54:34 +0800

    Add global snack bar utility and enhance existing snack bar method

--- lib/core/utils/helpers.dart
+++ lib/core/utils/helpers.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+final rootScaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+
 class AppHelpers {
   // Private constructor prevents instantiation of this class
   AppHelpers._();
 
+  static void showGlobalSnackBar(String message) {
+    rootScaffoldMessengerKey.currentState
+      ?..clearSnackBars()
+      ..showSnackBar(SnackBar(content: Text(message)));
+  }
+
   static void showSnackBar(BuildContext context, String message) {
-    ScaffoldMessenger.of(
-      context,
-    ).showSnackBar(SnackBar(content: Text(message)));
+    ScaffoldMessenger.of(context)
+      ..clearSnackBars()
+      ..showSnackBar(SnackBar(content: Text(message)));
   }
 }
