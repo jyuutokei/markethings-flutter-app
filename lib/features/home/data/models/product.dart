f1daab87b477e8a0e69a3c170e47ce80231d284c
furantuturu <furantuturu@proton.me>
2026-09-15 22:33:28 +0800

    Update product image paths to include 'sample' directory

--- lib/features/home/data/models/product.dart
+++ lib/features/home/data/models/product.dart
@@ -12,24 +12,24 @@ class ProductModel extends ProductEntity {
 
 List<ProductModel> demoProduct = [
   const ProductModel(
-    image: "assets/images/product_0.png",
+    image: "assets/images/sample/product_0.png",
     title: "Long Sleeve Shirts",
     price: 165,
     bgColor: Color(0xFFFEFBF9),
   ),
   const ProductModel(
-    image: "assets/images/product_1.png",
+    image: "assets/images/sample/product_1.png",
     title: "Casual Henley Shirts",
     price: 99,
   ),
   const ProductModel(
-    image: "assets/images/product_2.png",
+    image: "assets/images/sample/product_2.png",
     title: "Curved Hem Shirts",
     price: 180,
     bgColor: Color(0xFFF8FEFB),
   ),
   const ProductModel(
-    image: "assets/images/product_3.png",
+    image: "assets/images/sample/product_3.png",
     title: "Casual Nolin",
     price: 149,
     bgColor: Color(0xFFEEEEED),
