3e97a393bba83be57a60e2d3292b52ea52af500c
furantuturu <furantuturu@proton.me>
2026-09-15 22:33:39 +0800

    Refactor Home page layout: replace AppBar with HeaderAppbar and add SidebarNav

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:mt/core/constants/constants.dart';
 import 'package:mt/features/home/presentation/widgets/categories.dart';
+import 'package:mt/features/home/presentation/widgets/header_appbar.dart';
 import 'package:mt/features/home/presentation/widgets/new_arrival_products.dart';
 import 'package:mt/features/home/presentation/widgets/popular_products.dart';
 import 'package:mt/features/home/presentation/widgets/search_form.dart';
-import 'package:mt/injection_container.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:mt/features/home/presentation/widgets/sidebar.dart';
+import 'package:sidebarx/sidebarx.dart';
 import 'dart:async';
 
 class Home extends StatefulWidget {
@@ -18,6 +18,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool _isLoading = true;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final _sbxController = SidebarXController(selectedIndex: 0, extended: true);
 
   @override
   void initState() {
@@ -37,30 +39,11 @@ class _HomeState extends State<Home> {
     }
 
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            sl<SupabaseClient>().auth.signOut();
-          },
-          icon: SvgPicture.asset("assets/icons/menu.svg"),
-        ),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset("assets/icons/Location.svg"),
-            const SizedBox(width: defaultPadding / 2),
-            Text(
-              "Soon to be customized",
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-          ],
-        ),
-        actions: [
-          IconButton(
-            icon: SvgPicture.asset("assets/icons/Notification.svg"),
-            onPressed: () {},
-          ),
-        ],
+      key: _scaffoldKey,
+      drawer: SidebarNav(controller: _sbxController),
+      appBar: HeaderAppbar(
+        scaffoldKey: _scaffoldKey,
+        title: "Everything is a placeholder",
       ),
       body: const SingleChildScrollView(
         physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
@@ -68,7 +51,10 @@ class _HomeState extends State<Home> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Explore"),
+            Text(
+              "Explore",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
             Padding(
               padding: EdgeInsets.symmetric(vertical: defaultPadding),
               child: SearchForm(),
