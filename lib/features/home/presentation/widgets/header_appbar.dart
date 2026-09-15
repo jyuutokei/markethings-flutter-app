05ec5238d68e4409ac0855b5ad02499e45fc22bb
furantuturu <furantuturu@proton.me>
2026-09-15 22:31:06 +0800

    Reusable appbar

---
+++ lib/features/home/presentation/widgets/header_appbar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mt/core/constants/constants.dart';
+
+class HeaderAppbar extends StatelessWidget implements PreferredSizeWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey;
+  final String title;
+
+  const HeaderAppbar({
+    required this._scaffoldKey,
+    required this.title,
+    super.key,
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        onPressed: () {
+          _scaffoldKey.currentState?.openDrawer();
+        },
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset("assets/icons/Location.svg"),
+          const SizedBox(width: defaultPadding / 2),
+          Text(title, style: Theme.of(context).textTheme.bodyLarge),
+        ],
+      ),
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/Notification.svg",
+            colorFilter: const ColorFilter.mode(Colors.black, BlendMode.srcIn),
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
