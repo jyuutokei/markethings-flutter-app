1948e4c66581ea9fac4bc0c3b0daf35b8b21dd1b
furantuturu <furantuturu@proton.me>
2026-09-15 22:32:50 +0800

    Rearranged route constants and updated authenticated routes

--- lib/core/router/routes.dart
+++ lib/core/router/routes.dart
@@ -1,6 +1,11 @@
 abstract class AppRoute {
-  static const login = 'login';
   static const home = 'home';
+  static const cart = 'cart';
+  static const favorites = 'favorites';
+  static const profile = 'profile';
+  static const productDetails = 'product_details';
+
+  static const login = 'login';
   static const confirmEmailOtp = 'confirm_email_otp';
   static const resetPwdOtp = 'reset_pwd_otp';
   static const bridge = 'bridge';
@@ -8,5 +13,11 @@ abstract class AppRoute {
 
 abstract class AppRouteGuard {
   static const guestOnlyRoutes = {'/login', '/confirm_email_otp'};
-  static const authenticatedOnlyRoutes = {'/', '/bridge'};
+  static const authenticatedOnlyRoutes = {
+    '/',
+    '/cart',
+    '/favorites',
+    '/profile',
+    '/product_details',
+  };
 }
