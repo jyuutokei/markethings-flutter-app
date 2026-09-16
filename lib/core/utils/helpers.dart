e5a66746d8b3d83eba255308f6c9854fd9a5a01c
furantuturu <furantuturu@proton.me>
2026-09-16 22:11:22 +0800

    Refactor showGlobalSnackBar and showSnackBar to include title and contentType parameters for enhanced snackbar customization

--- lib/core/utils/helpers.dart
+++ lib/core/utils/helpers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 
 final rootScaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
@@ -6,15 +7,46 @@ class AppHelpers {
   // Private constructor prevents instantiation of this class
   AppHelpers._();
 
-  static void showGlobalSnackBar(String message) {
+  static void showGlobalSnackBar(
+    String title,
+    String message,
+    ContentType contentType,
+  ) {
     rootScaffoldMessengerKey.currentState
       ?..clearSnackBars()
-      ..showSnackBar(SnackBar(content: Text(message)));
+      ..showSnackBar(
+        SnackBar(
+          elevation: 0,
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Colors.transparent,
+          content: AwesomeSnackbarContent(
+            title: title,
+            message: message,
+            contentType: contentType,
+          ),
+        ),
+      );
   }
 
-  static void showSnackBar(BuildContext context, String message) {
+  static void showSnackBar(
+    BuildContext context,
+    String title,
+    String message,
+    ContentType contentType,
+  ) {
     ScaffoldMessenger.of(context)
       ..clearSnackBars()
-      ..showSnackBar(SnackBar(content: Text(message)));
+      ..showSnackBar(
+        SnackBar(
+          elevation: 0,
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Colors.transparent,
+          content: AwesomeSnackbarContent(
+            title: title,
+            message: message,
+            contentType: contentType,
+          ),
+        ),
+      );
   }
 }
