cb3d44600ebde9d9995670213ca0bbd53d781118
furantuturu <furantuturu@proton.me>
2026-09-17 22:35:24 +0800

    Refactor ProductEntity to use num for price and add quantity management methods

--- lib/features/home/domain/entities/product.dart
+++ lib/features/home/domain/entities/product.dart
@@ -4,15 +4,37 @@ import 'package:equatable/equatable.dart';
 
 class ProductEntity extends Equatable {
   final String image, title;
-  final int price;
+  final num price;
+  final int quantity;
   final Color bgColor;
 
   const ProductEntity({
     required this.image,
     required this.title,
     required this.price,
+    required this.quantity,
     this.bgColor = const Color(0xFFEFEFF2),
   });
+
+  ProductEntity quantityAdd() {
+    return copyWith(quantity: quantity + 1);
+  }
+
+  ProductEntity quantityReduce() {
+    if (quantity < 1) {
+      throw Exception("Quantity can't be zero");
+    }
+    return copyWith(quantity: quantity - 1);
+  }
+
+  ProductEntity copyWith({int? quantity}) {
+    return ProductEntity(
+      image: image,
+      title: title,
+      price: price,
+      quantity: quantity ?? this.quantity,
+    );
+  }
 
   @override
   List<Object?> get props => [image, title, price, bgColor];
