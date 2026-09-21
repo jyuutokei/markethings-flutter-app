eb7d417e776e13beb4ad3fcf778dc283245990bb
furantuturu <furantuturu@proton.me>
2026-09-21 22:00:14 +0800

    Refactor ProductCard to use StatefulWidget for favorite state management and improve layout structure

--- lib/features/home/presentation/widgets/product_card.dart
+++ lib/features/home/presentation/widgets/product_card.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
 import 'package:mt/core/constants/constants.dart';
+import 'package:mt/core/utils/helpers.dart';
 
-class ProductCard extends StatelessWidget {
-  const ProductCard({
+class ProductCard extends StatefulWidget {
+  ProductCard({
     super.key,
     required this.image,
     required this.title,
     required this.price,
     required this.press,
     required this.bgColor,
+    this.isFavorite = false,
   });
   final String image, title;
   final VoidCallback press;
   final num price;
+  bool isFavorite;
   final Color bgColor;
 
   @override
+  State<ProductCard> createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: press,
+      onTap: widget.press,
       child: Container(
         width: 154,
         padding: const EdgeInsets.all(defaultPadding / 2),
@@ -26,36 +36,53 @@ class ProductCard extends StatelessWidget {
           color: Colors.white,
           borderRadius: BorderRadius.all(Radius.circular(defaultBorderRadius)),
         ),
-        child: Column(
+        child: Stack(
           children: [
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: bgColor,
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(defaultBorderRadius),
-                ),
-              ),
-              child: Image.asset(image, height: 132),
-            ),
-            const SizedBox(height: defaultPadding / 2),
-            Row(
+            Column(
               children: [
-                Expanded(
-                  child: Text(
-                    title,
-                    style: const TextStyle(color: Colors.black),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: widget.bgColor,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(defaultBorderRadius),
+                    ),
                   ),
+                  child: Image.asset(widget.image, height: 132),
                 ),
-                const SizedBox(width: defaultPadding / 4),
-                Text(
-                  "\$$price",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
+                const SizedBox(height: defaultPadding / 2),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        widget.title,
+                        style: const TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    const Gap(defaultPadding / 4),
+                    Text(
+                      "\$${widget.price}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: AppHelpers.primaryColor(context),
+                      ),
+                    ),
+                  ],
                 ),
               ],
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: IconButton(
+                onPressed: () {
+                  setState(() => widget.isFavorite = !widget.isFavorite);
+                },
+                icon: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: SvgPicture.asset("assets/icons/Heart.svg", height: 20),
+                ),
+              ),
             ),
           ],
         ),
