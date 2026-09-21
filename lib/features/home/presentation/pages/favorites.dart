7c837ba470173ead4df3f0b4c3a6d8e1eb2a20fc
furantuturu <furantuturu@proton.me>
2026-09-21 21:59:21 +0800

    Implement Favorites page with GridView for displaying favorite products

--- lib/features/home/presentation/pages/favorites.dart
+++ lib/features/home/presentation/pages/favorites.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mt/core/constants/constants.dart';
+import 'package:mt/core/router/routes.dart';
+import 'package:mt/features/home/data/models/product.dart';
 import 'package:mt/features/home/presentation/widgets/header_appbar.dart';
+import 'package:mt/features/home/presentation/widgets/product_card.dart';
 import 'package:mt/features/home/presentation/widgets/sidebar.dart';
 import 'package:sidebarx/sidebarx.dart';
 
@@ -20,7 +25,35 @@ class _FavoritesState extends State<Favorites> {
       key: _scaffoldKey,
       drawer: SidebarNav(controller: _sbxController),
       appBar: HeaderAppbar(scaffoldKey: _scaffoldKey, title: "Favorites"),
-      body: const Center(child: Text("To be constructed")),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(defaultPadding),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.8,
+          crossAxisSpacing: defaultPadding,
+          mainAxisSpacing: defaultPadding,
+        ),
+        itemBuilder: (context, index) {
+          final product = demoProduct[index];
+
+          if (!product.isFavorite) {
+            return null;
+          }
+
+          return ProductCard(
+            title: demoProduct[index].title,
+            image: demoProduct[index].image,
+            price: demoProduct[index].price,
+            bgColor: demoProduct[index].bgColor,
+            press: () {
+              context.pushNamed(
+                AppRoute.productDetails,
+                extra: demoProduct[index],
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
