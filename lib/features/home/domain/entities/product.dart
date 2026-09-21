5767daf6bb22331af24217ff0b7e9c9b15147492
furantuturu <furantuturu@proton.me>
2026-09-21 21:58:25 +0800

    Refactor ProductEntity to simplify quantity management and remove unused methods

--- lib/features/home/domain/entities/product.dart
+++ lib/features/home/domain/entities/product.dart
@@ -5,36 +5,18 @@ import 'package:equatable/equatable.dart';
 class ProductEntity extends Equatable {
   final String image, title;
   final num price;
-  final int quantity;
   final Color bgColor;
+  int quantity;
+  bool isFavorite;
 
-  const ProductEntity({
+  ProductEntity({
     required this.image,
     required this.title,
     required this.price,
+    required this.bgColor,
     required this.quantity,
-    this.bgColor = const Color(0xFFEFEFF2),
+    required this.isFavorite,
   });
-
-  ProductEntity quantityAdd() {
-    return copyWith(quantity: quantity + 1);
-  }
-
-  ProductEntity quantityReduce() {
-    if (quantity < 1) {
-      throw Exception("Quantity can't be zero");
-    }
-    return copyWith(quantity: quantity - 1);
-  }
-
-  ProductEntity copyWith({int? quantity}) {
-    return ProductEntity(
-      image: image,
-      title: title,
-      price: price,
-      quantity: quantity ?? this.quantity,
-    );
-  }
 
   @override
   List<Object?> get props => [image, title, price, bgColor];
